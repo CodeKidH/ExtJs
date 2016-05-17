9c78534a17bb925d016cd34781805fdcd01cf33a
JeongHee <jeongh389@gmail.com>
2016-05-17 11:40:58 +0900

    final

--- MyExtJs5/app/view/chapter4/CustomTab.js
+++ MyExtJs5/app/view/chapter4/CustomTab.js
@@ -1,10 +1,10 @@
-
 Ext.define('ext5.view.chapter4.CustomTab', {
     extend: 'Ext.Component',
-    alias : 'widget.chapter4-customtab',
-    onRender: function () {     // #1
+    alias: 'widget.chapter4-customtab',
+
+    onRender: function () {
         this.callParent(arguments);
-        Ext.core.DomHelper.append(this.getEl(),     // #2
+        Ext.core.DomHelper.append(this.getEl(),
             '<div class="main_dashboard">' +
                 '<div class="tab_bg">' +
                 '<ul class="dashboard_tab_menu" id="ulroot">' +
@@ -12,102 +12,96 @@ Ext.define('ext5.view.chapter4.CustomTab', {
                 '</div>' +
                 '</div>'
         );
-
         this.setTabMenu();
     },
 
-    setTabMenu: function(){
-      var root = this.el.select('.dashboard_tab_menu').first();
-        var html = '<li><a href = "#" tabidx="{tabIdx}" class="{tabCls}">{tabName}</a></li>';
+    setTabMenu: function () {
+        var root = this.el.select('.dashboard_tab_menu').first();			// #1
+        var html = '<li><a href="#" tabidx="{tabIdx}" class="{tabCls}">{tabName}</a></li>'; // #2
+        var tpl = Ext.DomHelper.createTemplate(html);				// #3
 
-        var tpl = Ext.DomHelper.createTemplate(html);
-
-        Ext.Ajax.request({
-           url:"/resources/data/tablist.json",
-            method:"GET",
-            success: function(result, request){
-                var obj = Ext.JSON.decode(result.responseText);
-                Ext.each(obj.entitys, function(tabData){
-                    tpl.append(root,tabData);
+        Ext.Ajax.request({							// #4
+            url: "../../resources/data/tablist.json",							// #5
+            method: "GET",
+            success: function (result, request) {
+                var obj = Ext.JSON.decode(result.responseText);			// #6
+                Ext.each(obj.entitys, function (tabData) {			// #7
+                    tpl.append(root, tabData);					// #8
                 })
+            },
+            failure: function (result, request) {
+                Ext.Msg.alert("Failed", result.responseText);
             }
         });
     },
 
     initComponent: function () {
         var me = this;
-        me.callParent(arguments);
-        me.on('afterrender', function () {
+        this.callParent(arguments);
+        this.on('afterrender', function () {
             this.el.on("click", function (eventObject, htmlElement) {
-                eventObject.preventDefault();
-
+                eventObject.preventDefault();  // or eventObject.stopEvent();
                 Ext.select('.dashboard_tab_menu li a').removeCls('on');
-                Ext.get(htmlElement).addCls('on');
+                Ext.get(htmlElement).addCls("on");
 
                 var idx = Ext.get(htmlElement).getAttribute('tabidx');
-                me.fireEvent('tabselect',idx);
-
-            }, this,{
-                deletegate:"a"
+                me.fireEvent('tabselect', idx);
+            }, this, {
+                delegate: "a"
             });
 
-
-            this.el.on("contextmenu", function (eventObject, htmlElement) { //1
-                var menu = Ext.create('Ext.menu.Menu',{//2
-                    items:[
+            this.el.on("contextmenu", function (eventObject, htmlElement) {		// #1
+                var menu = Ext.create('Ext.menu.Menu', {				// #2
+                    items: [
                         {
-                            xtype:'menuitem',//3
-                            text:'delete',
-                            scope : me,
-                            handler: function(){ //4
-                                this.destroyTabMenu(htmlElement);//5
+                            xtype: 'menuitem',					// #3
+                            text: '삭제',
+                            scope: me,
+                            handler: function () {					// #4
+                                this.destroyTabMenu(htmlElement)			// #5
                             }
                         },
                         {
-                            xtype:'menuitem',
-                            text:'before add',
+                            xtype: 'menuitem',
+                            text: '이전에 추가',
                             scope: me,
-                            handler : function(){
-                                this.insertBeforeTabMenu(htmlElement);
+                            handler: function () {
+                                this.insertBeforeTabMenu(htmlElement)
                             }
                         },
                         {
-                            xtype:'menuitem',
-                            text:'after add',
+                            xtype: 'menuitem',
+                            text: '이후에 추가',
                             scope: me,
-                            handler:function(){
-                                this.insertAfterTabMenu(htmlElement);
+                            handler: function () {
+                                this.insertAfterTabMenu(htmlElement)
                             }
                         }
                     ]
                 });
-
-            eventObject.preventDefault();
-                menu.showBy(htmlElement);
-
+                eventObject.stopEvent();						// #6
+                menu.showBy(htmlElement);					// #7
             }, this, {
-                delegate: "a" ,  // #6
+                delegate: "a",
                 preventDefault: true
             });
         });
     },
 
-    destroyTabMenu:function(htmlElement){
-        Ext.get(htmlElement).destroy();
+    insertBeforeTabMenu: function (htmlElement) {
+        var root = this.el.select('.dashboard_tab_menu').first();		// #1
+        var insertBefore = Ext.get(htmlElement).up('li');			// #2
+
+        root.createChild('<li><a href="#" tabIdx="0" class="">이전 추가.</a></li>', insertBefore, true);						// #3
+        //Ext.DomHelper.insertBefore(insertBefore, '<li><a href="#" tabIdx="0" class="">이전 추가.</a></li>');							// #4
     },
 
-    insertBeforeTabMenu: function(htmlElement){
-        var root = this.el.select('.dashboard_tab_menu').first(); //1
-        var insertBefore = Ext.get(htmlElement).up('li'); //2
-
-        root.createChild('<li><a href="#" tabIdx="0" class="">before add</a></li>',insertBefore,true); //3
-
+    insertAfterTabMenu: function (htmlElement) {
+        var insertAfter = Ext.get(htmlElement).up('li');		// #1
+        Ext.DomHelper.insertAfter(insertAfter, '<li><a href="#" tabIdx="0" class="">이후에 추가.</a></li>');					// #2
     },
 
-    insertAfterTabMenu:function(htmlElement){
-        var insertAfter = Ext.get(htmlElement).up('li');
-        Ext.DomHelper.insertAfter(insertAfter, '<li><a href="#" tabIdx="0" class="">after add</a></li>')
+    destroyTabMenu: function (htmlElement) {
+        Ext.get(htmlElement).destroy();				// #1
     }
-
-
 });
