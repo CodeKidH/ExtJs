08d7f1368adddf78474f32e85b89d81f53fb52a1
JeongHee <jeongh389@gmail.com>
2016-05-27 11:11:01 +0900

    combo

---
+++ MyExtJs5/app/model/smpl/Code.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Administrator on 2016-05-27.
+ */
+Ext.define('ext5.model.smpl.Code',{
+    extend: 'Ext.data.Model',
+    fields:[
+        {
+            name:'cd_code',
+            type:'string'
+        },
+        {
+            name:'cd_desc',
+            type:'string'
+        }
+    ]
+});
